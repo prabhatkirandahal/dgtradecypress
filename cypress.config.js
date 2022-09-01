0457efa88cd5a1ad4c9069a9b515ad60f2379f25
prabhatkirandahal <prabhatkirandahal@gmail.com>
2022-09-01 14:59:11 +0545

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    // DG Trade baseUrl
+    baseUrl:'http://103.94.159.144:8080/BOM/web/index.html#',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
