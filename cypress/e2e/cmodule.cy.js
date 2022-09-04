5ded1f6a52fa7a2de3e32bf15f299bd5e7235af7
prabhatkirandahal <prabhatkirandahal@gmail.com>
2022-09-04 10:54:30 +0545

    client module

--- cypress/e2e/cmodule.cy.js
+++ cypress/e2e/cmodule.cy.js
@@ -1,19 +1,26 @@
 import {
+    // Importing login
     login
-}from "cypress\support\commands.js"
+}from "/cypress/support/commands.js"
 import cypressConfig from "../../cypress.config"
 
 describe("Client Module", ()=>{
 
     it("Login", ()=>{
-
+// Calling login.cy.js from commands.js
         login()
 
     })
 
     it("Client Module", ()=>{
+        login()
+        // disabling the hover feature of side menu bar
+
+        cy.get('.c-switch-slider').click()
         
-        cy.get('.c-show > .c-sidebar-nav-dropdown-toggle > .c-sidebar-nav-icon').click()
+        // cy.get('.c-header-toggler.ml-3 > .c-header-toggler-icon')
+        // cy.get('.c-sidebar-nav-dropdown-toggle').click()
+        // cy.get('#OAz\+TV25sfgHLqd4w8KvGw\=\=').click()
 
     })
 })
